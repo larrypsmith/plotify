a9f7c03833e5fcfd7f188780650eb90d3886e922
larrysmith <larrypsmith415@gmail.com>
2020-03-23 23:24:52 -0700

    Write GET request for artists

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import { redirectToLogin } from './util';
+import { redirectToLogin, requestTopArtists } from './util';
 import QueryString from 'query-string';
 
 document.addEventListener('DOMContentLoaded', () => {
   const welcome = document.getElementById('welcome');
-  const venn = document.getElementById('venn')
-  const parsed = QueryString.parse(window.location.search)
-  
+  const venn = document.getElementById('venn');
+  const parsed = QueryString.parse(window.location.search);
+
   if ('access_token' in parsed) {
-    welcome.setAttribute('style', 'display: none')
+    welcome.setAttribute('style', 'display: none');
+    const artists = requestTopArtists(parsed.access_token);
+    console.log(artists);
+
   } else {
-    venn.setAttribute('style', 'display: none')
+    venn.setAttribute('style', 'display: none');
     const loginButton = document.getElementById('login');
-    loginButton.addEventListener('click', redirectToLogin)
+    loginButton.addEventListener('click', redirectToLogin);
   }
-})
+});
--- src/util.js
+++ src/util.js
@@ -5,3 +5,16 @@ const generateRandomString = () => (
 export const redirectToLogin = () => {
   window.location.href = `https://accounts.spotify.com/authorize?client_id=45966386e108497e8a2e05195e9b94cc&response_type=token&redirect_uri=https://larrypsmith.github.io/plotify/&scope=user-top-read&state=${generateRandomString()}`
 }
+
+export const requestTopArtists = (accessToken) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.spotify.com/v1/me/top/artists`);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+  xhr.send();
+  xhr.onload(() => {
+    console.log(xhr.status);
+    console.log(xhr.responseType);
+    console.log(xhr.response);
+  });
+
+}
