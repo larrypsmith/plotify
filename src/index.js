0c411310ef9746b82c7c207683dbf12c9ac6a8c3
larrysmith <larrypsmith415@gmail.com>
2020-03-23 18:41:21 -0700

    add query string parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { redirectToLogin } from './spotifyApiUtil';
+import queryString from 'query-string';
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginButton = document.getElementById('login');
   loginButton.addEventListener('click', redirectToLogin)
+
+  const parsed = queryString.parse(window.location.href)
+  console.log(parsed)
 })
