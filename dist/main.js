f342575072bd18929448d7569fb3371e0ae2724a
larrysmith <larrypsmith415@gmail.com>
2020-03-23 18:16:39 -0700

    redirect simulates button click instead of http redirect

--- dist/main.js
+++ dist/main.js
@@ -106,7 +106,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _spo
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"redirectToLogin\", function() { return redirectToLogin; });\n// export const redirectToLogin = () => {\n//   const xhr = new XMLHttpRequest();\n//   xhr.open('GET',\n//     `https://accounts.spotify.com/authorize` +\n//     `?client_id=45966386e108497e8a2e05195e9b94cc` +\n//     `&response_type=token` +\n//     `&redirect_uri=https://larrypsmith.github.io/plotify/` +\n//     `&scope=user-top-read`,\n//     false\n//   )\n//   xhr.send()\n// }\n\nconst redirectToLogin = () => {\n  window.location.replace('https://accounts.spotify.com/authorize?client_id=45966386e108497e8a2e05195e9b94cc&response_type=token&redirect_uri=https://larrypsmith.github.io/plotify/&scope=user-top-read')\n}\n\n//# sourceURL=webpack:///./src/spotifyApiUtil.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"redirectToLogin\", function() { return redirectToLogin; });\nconst redirectToLogin = () => {\n  window.location.href = 'https://accounts.spotify.com/authorize?client_id=45966386e108497e8a2e05195e9b94cc&response_type=token&redirect_uri=https://larrypsmith.github.io/plotify/&scope=user-top-read'\n}\n\n//# sourceURL=webpack:///./src/spotifyApiUtil.js?");
 
 /***/ })
 
--- src/spotifyApiUtil.js
+++ src/spotifyApiUtil.js
@@ -1,16 +1,3 @@
-// export const redirectToLogin = () => {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET',
-//     `https://accounts.spotify.com/authorize` +
-//     `?client_id=45966386e108497e8a2e05195e9b94cc` +
-//     `&response_type=token` +
-//     `&redirect_uri=https://larrypsmith.github.io/plotify/` +
-//     `&scope=user-top-read`,
-//     false
-//   )
-//   xhr.send()
-// }
-
 export const redirectToLogin = () => {
-  window.location.replace('https://accounts.spotify.com/authorize?client_id=45966386e108497e8a2e05195e9b94cc&response_type=token&redirect_uri=https://larrypsmith.github.io/plotify/&scope=user-top-read')
+  window.location.href = 'https://accounts.spotify.com/authorize?client_id=45966386e108497e8a2e05195e9b94cc&response_type=token&redirect_uri=https://larrypsmith.github.io/plotify/&scope=user-top-read'
 }
