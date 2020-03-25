b5359c5847e55368dbd4c5121420511cad45e3de
larrysmith <larrypsmith415@gmail.com>
2020-03-24 19:41:56 -0700

    Move most of the logic to index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+import { redirectToLogin } from './util'
+import QueryString from 'query-string'
+
 import Venn from './venn';
 import Welcome from './welcome';
+
 
 const isAuthenticated = () => {
   const hash = window.location.hash.substr(1);
   return hash.includes('access_token');
+}
+
+const getAccessToken = () => {
+  const hash = window.location.hash.substr(1);
+  const parsedHash = QueryString.parse(hash);
+  return parsedHash.access_token;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,11 +21,22 @@ document.addEventListener('DOMContentLoaded', () => {
   main.innerHTML = "";
   
   if (isAuthenticated()) {
-    debugger
-    const venn = new Venn();
-    main.appendChild(venn.render);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.spotify.com/v1/me/top/artists`);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + getAccessToken());
+    xhr.onload = function () {
+      const response = JSON.parse(xhr.response);
+      const artists = response.items.map(artist => ({
+        name: artist.name,
+        genres: artist.genres
+      }));
+      const venn = new Venn(artists);
+      main.appendChild(venn.render())
+    }
+    xhr.send();
   } else {
-    debugger
     main.appendChild(Welcome.render())
+    const loginButton = document.querySelector('button');
+    loginButton.addEventListener('click', redirectToLogin);
   }
 })
--- src/venn.js
+++ src/venn.js
@@ -1,41 +1,14 @@
-import QueryString from 'query-string';
-
 export default class Venn {
-  constructor() {
-    debugger
-    this.accessToken = this.getAccessToken();
-    this.fetchArtists();
-  }
-
-  getAccessToken() {
-    const hash = window.location.hash.substr(1);
-    const parsedHash = QueryString.parse(hash);
-    return parsedHash.access_token;
-  }
-
-  fetchArtists() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.spotify.com/v1/me/top/artists`);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + this.accessToken);
-    xhr.onload = function() {
-      debugger
-      const response = JSON.parse(xhr.response);
-      this.artists = response.items.map(artist => ({
-        name: artist.name,
-        genres: artist.genres
-      }));
-    }
-    xhr.send();
+  constructor(artists) {
+    this.artists = artists;
   }
 
   vennHtml() {
-    debugger
-    const html = this.artists;
+    const html = this.artists.map(artist => artist.name);
     return html;
   }
 
   render() {
-    debugger
     let container = document.createElement("div");
     container.className = "venn";
     container.innerHTML = this.vennHtml();
