f0e8c653688109271bcbac0cb2296b5ae37d4d59
larrysmith <larrypsmith415@gmail.com>
2020-03-26 12:44:33 -0700

    Replace xmlhttprequest with fetch API

--- dist/main.js
+++ dist/main.js
@@ -29979,64 +29979,84 @@ __webpack_require__.r(__webpack_exports__);
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   main.innerHTML = "";
-  
+
   if (Object(_util__WEBPACK_IMPORTED_MODULE_0__["isAuthenticated"])()) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.spotify.com/v1/me/top/artists?limit=50`);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + Object(_util__WEBPACK_IMPORTED_MODULE_0__["getAccessToken"])());
-    xhr.onload = function () {
-      const response = JSON.parse(xhr.response);
-      const artists = response.items.map(artist => ({
-        name: artist.name,
-        genres: artist.genres
-      }));
-
-      const chart = new _BubbleChart__WEBPACK_IMPORTED_MODULE_1__["default"](artists);
-      const genres = chart.genres;
-      const root = {children: genres};
-      let hierarchy = d3__WEBPACK_IMPORTED_MODULE_3__["hierarchy"](root).count();
-      const width = 500;
-      const height = width;
-
-      let pack = d3__WEBPACK_IMPORTED_MODULE_3__["pack"]()
-        .size([width, height])
-        .padding(2)
-      let packedData = pack(hierarchy);
-
-      const main = d3__WEBPACK_IMPORTED_MODULE_3__["select"]('main')
-
-      const svg = main.append("svg")
-        .style('width', `${width}px`)
-        .style('height', `${height}px`)
-        .attr('font-size', 10)
-        .attr('font-family', 'sans-serif')
-        .attr('text-anchor', 'middle');
-
-      const color = d3__WEBPACK_IMPORTED_MODULE_3__["scaleLinear"]()
-        .domain([0, packedData.height])
-        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-        .interpolate(d3__WEBPACK_IMPORTED_MODULE_3__["interpolateHcl"])
-
-      const node = svg.append('g')
-        .selectAll('circle')
-        .data(packedData.descendants().slice(1))
-        .join('circle')
-          .attr('fill', d => d.children ? color(d.depth) : "white")
-          .attr('transform', d => `translate(${d.x + 1}, ${d.y + 1})`)
-          .attr('r', d => `${d.r}`)
-
-      debugger
-
-    }
-    xhr.send();
+    debugger
+    const accessToken = Object(_util__WEBPACK_IMPORTED_MODULE_0__["getAccessToken"])();
+    fetch(`https://api.spotify.com/v1/me/top/artists?limit=50`, {
+      method: 'GET',
+      headers: { 'Authorization': `Bearer ${accessToken}`}
+    })
+      .then(res => console.log(res))
+      .then(data => console.log(data))
   } else {
     main.appendChild(_Welcome__WEBPACK_IMPORTED_MODULE_2__["default"].render())
     const loginButton = document.querySelector('button');
     loginButton.addEventListener('click', _util__WEBPACK_IMPORTED_MODULE_0__["redirectToLogin"]);
   }
 })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const main = document.querySelector('main')
+//   main.innerHTML = "";
+  
+//   if (isAuthenticated()) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `https://api.spotify.com/v1/me/top/artists?limit=50`);
+//     xhr.setRequestHeader('Authorization', 'Bearer ' + getAccessToken());
+//     xhr.onload = function () {
+//       const response = JSON.parse(xhr.response);
+//       const artists = response.items.map(artist => ({
+//         name: artist.name,
+//         genres: artist.genres
+//       }));
+
+//       const chart = new BubbleChart(artists);
+//       const genres = chart.genres;
+//       const root = {children: genres};
+//       let hierarchy = d3.hierarchy(root).count();
+//       const width = 500;
+//       const height = width;
+
+//       let pack = d3.pack()
+//         .size([width, height])
+//         .padding(2)
+//       let packedData = pack(hierarchy);
+
+//       const main = d3.select('main')
+
+//       const svg = main.append("svg")
+//         .style('width', `${width}px`)
+//         .style('height', `${height}px`)
+//         .attr('font-size', 10)
+//         .attr('font-family', 'sans-serif')
+//         .attr('text-anchor', 'middle');
+
+//       const color = d3.scaleLinear()
+//         .domain([0, packedData.height])
+//         .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+//         .interpolate(d3.interpolateHcl)
+
+//       const node = svg.append('g')
+//         .selectAll('circle')
+//         .data(packedData.descendants().slice(1))
+//         .join('circle')
+//           .attr('fill', d => d.children ? color(d.depth) : "white")
+//           .attr('transform', d => `translate(${d.x + 1}, ${d.y + 1})`)
+//           .attr('r', d => `${d.r}`)
+
+//       debugger
+
+//     }
+//     xhr.send();
+//   } else {
+//     main.appendChild(Welcome.render())
+//     const loginButton = document.querySelector('button');
+//     loginButton.addEventListener('click', redirectToLogin);
+//   }
+// })
 
   
 
--- src/index.js
+++ src/index.js
@@ -6,63 +6,83 @@ import Welcome from './Welcome';
 import * as d3 from 'd3';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   main.innerHTML = "";
-  
+
   if (isAuthenticated()) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.spotify.com/v1/me/top/artists?limit=50`);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + getAccessToken());
-    xhr.onload = function () {
-      const response = JSON.parse(xhr.response);
-      const artists = response.items.map(artist => ({
-        name: artist.name,
-        genres: artist.genres
-      }));
-
-      const chart = new BubbleChart(artists);
-      const genres = chart.genres;
-      const root = {children: genres};
-      let hierarchy = d3.hierarchy(root).count();
-      const width = 500;
-      const height = width;
-
-      let pack = d3.pack()
-        .size([width, height])
-        .padding(2)
-      let packedData = pack(hierarchy);
-
-      const main = d3.select('main')
-
-      const svg = main.append("svg")
-        .style('width', `${width}px`)
-        .style('height', `${height}px`)
-        .attr('font-size', 10)
-        .attr('font-family', 'sans-serif')
-        .attr('text-anchor', 'middle');
-
-      const color = d3.scaleLinear()
-        .domain([0, packedData.height])
-        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-        .interpolate(d3.interpolateHcl)
-
-      const node = svg.append('g')
-        .selectAll('circle')
-        .data(packedData.descendants().slice(1))
-        .join('circle')
-          .attr('fill', d => d.children ? color(d.depth) : "white")
-          .attr('transform', d => `translate(${d.x + 1}, ${d.y + 1})`)
-          .attr('r', d => `${d.r}`)
-
-      debugger
-
-    }
-    xhr.send();
+    debugger
+    const accessToken = getAccessToken();
+    fetch(`https://api.spotify.com/v1/me/top/artists?limit=50`, {
+      method: 'GET',
+      headers: { 'Authorization': `Bearer ${accessToken}`}
+    })
+      .then(res => console.log(res))
+      .then(data => console.log(data))
   } else {
     main.appendChild(Welcome.render())
     const loginButton = document.querySelector('button');
     loginButton.addEventListener('click', redirectToLogin);
   }
 })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const main = document.querySelector('main')
+//   main.innerHTML = "";
+  
+//   if (isAuthenticated()) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `https://api.spotify.com/v1/me/top/artists?limit=50`);
+//     xhr.setRequestHeader('Authorization', 'Bearer ' + getAccessToken());
+//     xhr.onload = function () {
+//       const response = JSON.parse(xhr.response);
+//       const artists = response.items.map(artist => ({
+//         name: artist.name,
+//         genres: artist.genres
+//       }));
+
+//       const chart = new BubbleChart(artists);
+//       const genres = chart.genres;
+//       const root = {children: genres};
+//       let hierarchy = d3.hierarchy(root).count();
+//       const width = 500;
+//       const height = width;
+
+//       let pack = d3.pack()
+//         .size([width, height])
+//         .padding(2)
+//       let packedData = pack(hierarchy);
+
+//       const main = d3.select('main')
+
+//       const svg = main.append("svg")
+//         .style('width', `${width}px`)
+//         .style('height', `${height}px`)
+//         .attr('font-size', 10)
+//         .attr('font-family', 'sans-serif')
+//         .attr('text-anchor', 'middle');
+
+//       const color = d3.scaleLinear()
+//         .domain([0, packedData.height])
+//         .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+//         .interpolate(d3.interpolateHcl)
+
+//       const node = svg.append('g')
+//         .selectAll('circle')
+//         .data(packedData.descendants().slice(1))
+//         .join('circle')
+//           .attr('fill', d => d.children ? color(d.depth) : "white")
+//           .attr('transform', d => `translate(${d.x + 1}, ${d.y + 1})`)
+//           .attr('r', d => `${d.r}`)
+
+//       debugger
+
+//     }
+//     xhr.send();
+//   } else {
+//     main.appendChild(Welcome.render())
+//     const loginButton = document.querySelector('button');
+//     loginButton.addEventListener('click', redirectToLogin);
+//   }
+// })
 
   
