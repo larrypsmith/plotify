f5c69199033a0d0836c264345655c7943afc27e5
larrysmith <larrypsmith415@gmail.com>
2020-04-30 09:31:38 -0700

    Debug redirect URI issue

--- src/util.js
+++ src/util.js
@@ -1,13 +1,14 @@
 import { descending } from 'd3';
 import QueryString from 'query-string';
 
-export const redirectToLogin = () => {
+export const redirectToLogin = (state) => {
   window.location.href = 'https://accounts.spotify.com/authorize' +
   '?client_id=45966386e108497e8a2e05195e9b94cc' +
   '&response_type=token' + 
   '&redirect_uri=http://larrysmith.me/plotify' +
   '&scope=user-top-read' +
-  '&limit=50'
+  '&limit=50' + 
+  `state=${state}`
 }
 
 export const isAuthenticated = () => {
