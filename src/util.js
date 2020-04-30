ee9922ca38049ba95ef78194c7318d0959abc446
larrysmith <larrypsmith415@gmail.com>
2020-04-30 11:14:13 -0700

    Add / to end of redirect URI

--- src/util.js
+++ src/util.js
@@ -5,7 +5,7 @@ export const redirectToLogin = (state) => {
   window.location.href = 'https://accounts.spotify.com/authorize' +
   '?client_id=45966386e108497e8a2e05195e9b94cc' +
   '&response_type=token' + 
-  '&redirect_uri=http://larrysmith.me/plotify' +
+  '&redirect_uri=http://larrysmith.me/plotify/' +
   '&scope=user-top-read' +
   '&limit=50' + 
   `&state=${state}`
