86a250532410adf86f1092e807a01c9a8ed3158c
larrysmith <larrypsmith415@gmail.com>
2020-04-29 23:32:10 -0700

    Change redirect URI to larrysmith.me/plotify

--- src/util.js
+++ src/util.js
@@ -5,7 +5,7 @@ export const redirectToLogin = () => {
   window.location.href = 'https://accounts.spotify.com/authorize' +
   '?client_id=45966386e108497e8a2e05195e9b94cc' +
   '&response_type=token' + 
-  '&redirect_uri=http://larrypsmith.github.io/plotify' +
+  '&redirect_uri=http://larrysmith.me/plotify' +
   '&scope=user-top-read' +
   '&limit=50'
 }
